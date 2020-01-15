f8edfa52a2a0bfa34bf8070d1c82b58a4f4dc19d
Angelina Baranova <gerlinda137@gmail.com>
2020-01-15 12:59:09 +0300

    исправлены отступы в контактах и сделана точка афтер в личном каб

--- js/script.js
+++ js/script.js
@@ -41,19 +41,3 @@ leftArrow.onclick = toggleFunctionArrow;
     toggleFunctionArrow();
   };
 });*/
-
-//скрипты catalog.html
-
-let toCart = document.querySelectorAll(".to-buy");
-let toCartModal = document.querySelector(".added-to-basket__hidden");
-
-toCart.forEach(function(item) {
-  item.addEventListener("click", function(){
-    toCartModal.classList.add("modal-show");
-  });
-});
-
-/*let closeButton = document.querySelector(".close-button");
-closeButton.addEventListener("click", function(){
-  toCartModal.classList.remove("modal-show");
-});*/
